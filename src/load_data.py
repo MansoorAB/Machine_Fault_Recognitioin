efa59d70f00742d5bc19861de67bca39c5eaf723
MansoorAB <baig.mansoorali@gmail.com>
2021-07-30 16:57:04 +0530

    logging mechanism implemented

--- src/load_data.py
+++ src/load_data.py
@@ -1,12 +1,13 @@
-# Copy the data from the source and save it in the raw files folder
-# Takes arguments
+"""
+    This module copies the data from source location to raw folder.
+"""
 
 import yaml
 import argparse
 import os
 import glob
 import shutil
-from os import listdir
+from application_logging import logger
 
 
 def read_params(config_path):
@@ -20,15 +21,25 @@ def load_data(config_path):
     source_data = config['data_source']['s3_source']
     raw_data = config['load_data']['raw_data']
 
+    if os.path.isdir(raw_data):
+        shutil.rmtree(raw_data)
+        log_writer.log(file_object, "Deleted existing directory: {}".format(raw_data))
+
+    os.makedirs(raw_data)
+    log_writer.log(file_object, "Created new directory: {}".format(raw_data))
+
     csv_files = glob.glob(os.path.join(source_data, "*.csv"))
-    # files = os.listdir(source_data)
-    os.makedirs(raw_data, exist_ok=True)
     for file in csv_files:
         shutil.copy(file, raw_data)
+        log_writer.log(file_object, "copying file {} to {}".format(file, raw_data))
 
 
 if __name__ == "__main__":
+    log_writer = logger.App_Logger()
+    file_object = open("Training_Logs/load_data_log.txt", "w")
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
+
+    log_writer.log(file_object, "Load data started with params file: {}".format(parsed_args.config))
     load_data(config_path=parsed_args.config)
