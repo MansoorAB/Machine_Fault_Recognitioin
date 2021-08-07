9d52f5969ac86d67c4cfc23e359e62ede10c1ea9
MansoorAB <baig.mansoorali@gmail.com>
2021-08-07 17:49:11 +0530

    app.py updated

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def predictWaferStatus():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
